b13411b3af5748cb3b1c0e62ef9e84eef13a132a
joy0124 <22400009@handong.ac.kr>
2025-12-17 19:02:23 +0900

    지도 완성

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,21 +1,24 @@
-<<<<<<< HEAD
 import KakaoMap from "../components/KakaoMap";
-=======
->>>>>>> 253fb87 (15주차 과제 완료)
+
 function MapPage() {
-    return (
-        <section className="section">
-            <h1 className="section-title">캠퍼스 지도에서 식당 찾기</h1>
-            <p className="section-sub">지도를 보고 식당 위치를 확인하세요.</p>
-            <div className="map-container">
-<<<<<<< HEAD
-               <KakaoMap />
-=======
-                <img src="/campus-map.png" className="campus-map" alt="캠퍼스 지도" onError={(e) => e.target.style.display = 'none'} />
->>>>>>> 253fb87 (15주차 과제 완료)
-            </div>
-        </section>
-    );
+  return (
+    <section className="section">
+      <h1 className="section-title">캠퍼스 지도에서 식당 찾기</h1>
+      <p className="section-sub">지도를 보고 식당 위치를 확인하세요.</p>
+
+      <div className="map-container">
+        <KakaoMap />
+        <img
+          src="/campus-map.png"
+          className="campus-map"
+          alt="캠퍼스 지도"
+          onError={(e) => {
+            e.currentTarget.style.display = "none";
+          }}
+        />
+      </div>
+    </section>
+  );
 }
 
 export default MapPage;
